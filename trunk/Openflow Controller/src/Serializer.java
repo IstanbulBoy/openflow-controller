3b78a35993aa7c815205312e81cea0967fcba970
faustoal23@gmail.com <faustoal23@gmail.com@49498064-44d4-63bd-a392-90e5ef376c67>
2014-01-27 14:45:20 +0000

    Interfaz de Usuario para administrar y monitorear flujos con Floodlight.

---
+++ trunk/Openflow Controller/src/Serializer.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+    Serializer.java
+    Esta clase actualmente solo tiene un metodo (installFlow) que recibe
+    una URL enf orma de String y un String que se genera a partir de un
+    ojeto JSON que posee los parametros para configurar el flujo.
+*/
+
+/**
+ *
+ * @author fausto
+ */
+public class Serializer {
+  
+    public static void installFlow(final String surl, final String flow) {
+        try {
+            URL url = new URL(surl);
+            HttpURLConnection connection = null;
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setUseCaches(false);
+
+            
+            OutputStream stream = connection.getOutputStream();
+            stream.write(flow.getBytes());
+            stream.close();
+            
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuffer sb = new StringBuffer();
+            String str = br.readLine();
+            
+            while(str!=null) {
+                sb.append(str);
+                str = br.readLine();
+            }
+            
+            System.out.println("Response" + sb);
+        } catch (IOException e) {
+            System.out.println("Failed to deserialize data" + " : readJsonObjectFromURL");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ trunk/Openflow Controller/src/test.java
@@ -0,0 +1,88 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+    test.java
+    Archivo main de prueba para verificar el correcto funcionamiento de los 
+    metodos hasta la fecha (Deserializer.java, FloodlightProvider.java y
+    Serializer.java)
+*/
+
+/**
+ *
+ * @author fausto
+ */
+public class test {
+    public static void main(String[] args) throws JSONException {
+        String IP = "10.0.0.238";
+        String PORT = "8080";
+        FloodlightProvider.setIP(IP);       // Set IP en FloodlightProvider
+        FloodlightProvider.setPort(PORT);   // Set PORT en FloodlightProvider
+        JSONArray Switches;                 // Crea arreglo JSON
+        JSONObject Health,Memory,Modules;   // Crea objetos JSON
+        
+        // Se envian los URL a Deserializer y se almacena la respuesta en las
+        // variables creadas anteriormente.
+        Health = Deserializer.readJsonObjectFromURL("http://" + IP
+				+ ":" + PORT + "/wm/core/health/json");
+        Memory = Deserializer.readJsonObjectFromURL("http://" + IP
+				+ ":" + PORT + "/wm/core/memory/json");
+	Modules = Deserializer.readJsonObjectFromURL("http://" + IP
+				+ ":" + PORT + "/wm/core/module/loaded/json");
+        Switches = Deserializer.readJsonArrayFromURL("http://" + IP
+				+ ":" + PORT + "/wm/core/controller/switches/json");
+        
+      
+        // Se imprimen los resultados
+        System.out.println(Health);
+        System.out.println(Memory);
+        System.out.println(Modules);
+        System.out.println(Switches);
+        System.out.println("");
+        System.out.println(Health.get("healthy"));
+        
+        // FLOW SET UP. 
+        // Primero se crean los objetos JSON para configurar los flujos.
+        // Luego se convierten en String.
+        // Luego se envian los flujos con su respectiva URL a Serializer
+        String flow1 = new JSONStringer().object()
+                .key("switch").value("00:00:00:00:00:00:00:01")
+                .key("name").value("flow-mod-1")
+                .key("cookie").value("0")
+                .key("priority").value("32768")
+                .key("ingress-port").value("1")
+                .key("active").value("true")
+                .key("actions").value("output=2")
+                .endObject().toString();
+        
+        String flow2 = new JSONStringer().object()
+                .key("switch").value("00:00:00:00:00:00:00:01")
+                .key("name").value("flow-mod-2")
+                .key("cookie").value("0")
+                .key("priority").value("32768")
+                .key("ingress-port").value("2")
+                .key("active").value("true")
+                .key("actions").value("output=1")
+                .endObject().toString();
+        
+        Serializer.installFlow("http://" + IP
+				+ ":" + PORT + "/wm/staticflowentrypusher/json", flow1);
+        Serializer.installFlow("http://" + IP
+				+ ":" + PORT + "/wm/staticflowentrypusher/json", flow2);
+        
+       
+        
+        
+        
+        //System.out.println(flow1);
+        //System.out.println(flow2);
+    }
+}
